94cab71a2cacb18e8a91d3483aa112a33f50a02e
Arian Johnson <leshaun.john@gmail.com>
2018-03-11 18:05:46 -0600

    Add files via upload

---
+++ router/index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	Help: 	 require('./lib/help.js'),
+	Routes:  require('./lib/Routes.js'),
+	Session: require('./lib/Session.js'),
+	REST: 	 require('./lib/rest.js'),
+	Errors:  require('./lib/errors.js'),
+}
