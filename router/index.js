59c56c4bfd959f203e47aebc7a7b93feba9edac0
Arian Johnson <leshaun.john@gmail.com>
2018-03-11 18:08:13 -0600

    Delete index.js

--- router/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-	Help: 	 require('./lib/help.js'),
-	Routes:  require('./lib/Routes.js'),
-	Session: require('./lib/Session.js'),
-	REST: 	 require('./lib/rest.js'),
-	Errors:  require('./lib/errors.js'),
-}
