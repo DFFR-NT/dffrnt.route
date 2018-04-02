067b83ee47807230073900a421ad7aaf117276c2
Arian Johnson <leshaun.john@gmail.com>
2018-04-01 19:32:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 // ----------------------------------------------------------------------------------------------
 // Handle Requires ------------------------------------------------------------------------------
 
-	import { default as Help 	} from './lib/help.js';
-	import { default as Routes 	} from './lib/Routes.js';
-	import { default as Session } from './lib/Session.js';
-	import { default as REST 	} from './lib/rest.js';
-	import { default as Errors 	} from './lib/errors.js';
-	export { Help, Routes, Session, REST, Errors };
-
-	export default { Help, Routes, Session, REST, Errors };
+	module.exports = {
+		Help: 		require('./lib/help.js'),
+		Routes: 	require('./lib/Routes.js'),
+		Session: 	require('./lib/Session.js'),
+		REST: 		require('./lib/rest.js'),
+		Errors: 	require('./lib/errors.js')
+	};
